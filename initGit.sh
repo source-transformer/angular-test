d4c99965d03b4c4f6def23e2983a283ee74c97d3
source-transformer <tcassidy@kabam.com>
2019-05-04 19:17:27 -0700

    13 minutes into this tutorial: https://www.youtube.com/watch?v=5wtnKulcquA

---
+++ initGit.sh
@@ -0,0 +1,6 @@
+echo "# angular-test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/source-transformer/angular-test.git
+git push -u origin master
