bd8dbb755c5b5d10dc6959afbff233aca52f4819
source-transformer <tcassidy@kabam.com>
2019-06-02 11:09:11 -0700

    client: change display of text based on boolean in component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  h1Style: boolean = false;
 
   constructor() { }
 
@@ -14,6 +16,7 @@ export class HomeComponent implements OnInit {
 
   firstClick(){
     console.log('clicked');
+    this.h1Style = true;
   }
 
 }
